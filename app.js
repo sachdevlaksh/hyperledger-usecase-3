68ac7da0d8d16253d6c83f16aa993033340780a2
Lakshay Sachdeva <35967999+sachdevlaksh@users.noreply.github.com>
2019-01-29 18:01:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+var myApp = angular.module("myModule", []);
+
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileAndFieldsToUrl = function (file, fields, uploadUrl) {
+        var fd = new FormData();    // what FormData do
+        fd.append('file', file);    // what is the logic behind this
+        for (var i = 0; i < fields.length; i++) {
+            fd.append(fields[i].name, fields[i].data);  // what is the logic behind this
+        }
+        $http({
+            method: 'POST',
+            url: uploadUrl,  //how only half url work fine we should have complete URL
+            data: fd,
+            transformRequest: angular.identity,    //what this will do angular.identity
+            headers: {
+                'Content-Type': undefined
+            }
+        }).then(function successCallback(response) {
+            if (JSON.stringify(response) != '{}' && response.data.status == "200") {
+                window.location.href = '/SuccessEntry.html';
+            } else {
+                alert(response.data.message);
+            }
+        });
+    }
+}]);
+
+myApp.controller('myController', ['$scope', 'fileUpload', '$http', '$filter', '$window', function ($scope, fileUpload, $http, $filter, $window) {
+
+    function dateTime() {
+        var now = new Date();
+        return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + "T" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    }
+    $scope.gender = ["Male", "Female"];
+    $scope.type = ["Full-Type", "Part-Type"];
+    $scope.ctc = ["Less than 5 LPA", "5 - 10 LPA", "Greater than 10 LPA"];
+    var uniqueId = Date.now();
+
+
+    var timestamp = dateTime();
+
+    var Employee = { CurrentEmployer: "", PreviousEmployer: "", TotalExperience: "", CurrentCTC: "" }
+    var Visa = { Country: "", Duration: "", ReasonOfTraveling: "", Comments: "", Status: "" }
+    var Student = { HighestEducation: "", CourseToPursue: "", Specialization: "", Type: "" }
+    var User = { _id: uniqueId + '', Name: "", DOB: "", Age: "", MobileNumber: "", Gender: "", Email: "", GovermentId: "", Address: "", registrationTimeStamp: timestamp, employee: Employee, visa: Visa, student: Student }
+
+
+    $scope.User = User;
+
+    $scope.submitLoginData = function () {
+        var data = {
+            username: $scope.userName,
+            password: $scope.password
+        }
+
+
+        $http({
+            method: 'POST',
+            url: '/loginData',
+            data: data
+        }).then(function successCallback(response) {
+            if (JSON.stringify(response) != '{}' && response.data.status == "200") {
+                window.location.href = '/ApplicationDetails.html';
+            } else {
+                alert(response.data.message);
+            }
+        });
+    }
+
+    $scope.submitUserData = function () {
+
+        var dob = $scope.User.DOB;
+        var year = Number(dob.substr(6,4));
+        var month = Number(dob.substr(3, 2)) - 1;
+        var day = Number(dob.substr(0, 2));
+        var today = new Date();
+        var age = today.getFullYear() - year;
+        if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+            age--;
+        }
+        $scope.User.Age = age;
+        console.log($scope.User);
+        $http({
+            method: 'POST',
+            url: '/applicantData',
+            data: $scope.User
+        }).then(function successCallback(response) {
+            if (JSON.stringify(response) != '{}' && response.data.status == "200") {
+				window.location.href = '/SuccessEntry.html';
+                var fields = [{
+                    "name": "id",
+                    "data": response.data.id
+                },
+                {
+                    "name": "rev",
+                    "data": response.data.revid
+                }
+                ];
+                var uploadUrl = "/applicantDoc";
+                fileUpload.uploadFileAndFieldsToUrl(file, fields, uploadUrl);
+            } else {
+                alert(response.data.message);
+            }
+        });
+    }
+	
+    $scope.submitEmployeeData = function () {
+
+        console.log($scope.User);
+        $http({
+            method: 'POST',
+            url: '/employeeData',
+            data: $scope.User
+        }).then(function successCallback(response) {
+            if (JSON.stringify(response) != '{}' && response.data.status == "200") {
+                var fields = [{
+                    "name": "id",
+                    "data": response.data.id
+                },
+                {
+                    "name": "rev",
+                    "data": response.data.revid
+                }
+                ];
+                var uploadUrl = "/applicantDoc";
+                fileUpload.uploadFileAndFieldsToUrl(file, fields, uploadUrl);
+            } else {
+                alert(response.data.message);
+            }
+        });
+    }
+}]);
---
+++ server.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var path = require('path');
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var port = process.env.PORT || process.env.VCAP_APP_PORT || '8083';
+var nano = require('nano')('http://localhost:'+port);            
+var app = express();
+var multer  = require('multer');         
+app.use(bodyParser.json());                
+app.use(bodyParser.urlencoded({ extended: true }));        
+
+var upload = multer({dest:__dirname + '/upload'});            
+var type = upload.single('file');                             
+
+app.use('/', express.static(__dirname + '/'));                 
+app.use('/', express.static(__dirname +'/Images'));            
+
+var cloudantUserName = "adfc60ec-fa4a-46a9-bcf9-e554de84b30b-bluemix";
+var cloudantPassword = "0f130af356c2fcfc117f121fa37673bf41c604165888b98399dfd6bcf62dacaa";
+var dbCredentials_url = "https://"+cloudantUserName+":"+cloudantPassword+"@"+cloudantUserName+".cloudant.com"; 
+
+//Initialize the library with my account
+var cloudant = require('cloudant')(dbCredentials_url);               
+
+var dbForLogin = cloudant.db.use("logindetails");
+var dbForApplicantData = cloudant.db.use("digitalid");
+var dbForAdminRequestTable = cloudant.db.use("adminrequesttable");
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    console.log("Open LoginPage.html page");
+    res.sendFile(path.join(__dirname + '/PSKLogin.html'));
+});
+
+app.post('/loginData', function(req, res) {
+    console.log("Got a POST request for LoginPage.html page");
+    var userName = req.body.username;
+    var password = req.body.password;
+    dbForLogin.get(userName, function(err, body) {
+        if (!err) {
+            var dbPassword = body.agentPassword;
+            if (dbPassword === password) {
+                var response = {
+                    status: 200,
+                    message: 'Success'
+                }
+                res.send(JSON.stringify(response));
+            } else {
+                var response = {
+                    status: 300,
+                    message: 'Username and Password does not match'
+                }
+                res.send(JSON.stringify(response));
+            }
+        } else {
+            var response = {
+                status: 400,
+                message: 'Username does not exists'
+            }
+            res.send(JSON.stringify(response));
+        }
+    });
+});
+
+app.post('/applicantData', function(req, res) {
+    var response = "";
+    console.log("Got a POST request for apply_for_digital_id.html page");
+    var applicantData = JSON.parse(JSON.stringify(req.body)); 
+	console.log(applicantData);
+	dbForApplicantData.insert(applicantData, function(err, body) {
+		if (!err) {
+			response = {
+				status: 200,
+				message: 'Data inserted successfully in applicant data table.',
+				id: body.id,
+				revid: body.rev
+			}
+		} else {
+			response = {
+				status: 300,
+				message: 'Data not inserted successfully in applicant data table.'
+			}
+		}
+		res.send(JSON.stringify(response));
+	});
+});
+
+app.post('/employeeData', function(req, res) {
+    var response = "";
+    console.log("Got a POST request for apply_for_digital_id.html page");
+    var applicantData = JSON.parse(JSON.stringify(req.body)); 
+	dbForApplicantData.insert(applicantData, function(err, body) {
+		if (!err) {
+			response = {
+				status: 200,
+				message: 'Data inserted successfully in applicant data table.',
+				id: body.id,
+				revid: body.rev
+			}
+		} else {
+			response = {
+				status: 300,
+				message: 'Data not inserted successfully in applicant data table.'
+			}
+		}
+		res.send(JSON.stringify(response));
+	});
+});
+app.post('/userData', function(req, res) {
+    var response = "";
+    console.log("Got a POST request for StudentDetails.html page");
+    var applicantData = JSON.parse(JSON.stringify(req.body)); 
+	dbForApplicantData.insert(applicantData, function(err, body) {
+		if (!err) {
+			response = {
+				status: 200,
+				message: 'Data inserted successfully in applicant data table.',
+				id: body.id,
+				revid: body.rev
+			}
+		} else {
+			response = {
+				status: 300,
+				message: 'Data not inserted successfully in applicant data table.'
+			}
+		}
+		res.send(JSON.stringify(response));
+	});
+});
+
+app.post('/applicantDoc', type, function(req, res) {
+    console.log("File :"+JSON.stringify(req.body));
+    fs.readFile(__dirname + '/upload/' + req.file.filename, function(err, data) {
+        if (!err) {
+            dbForApplicantData.attachment.insert(req.body.id, req.file.originalname, data, req.file.mimetype, {
+                rev: req.body.rev
+            }, function(err, body) {
+                if (!err) {
+                    fs.unlink(__dirname + '/upload/' + req.file.filename, function(err) {
+                        if (!err)
+                            console.log('File deleted!');
+                        else
+                            console.log(err);
+                    });
+                    var response = {
+                        status: 200,
+                        message: 'Document uploaded successfully in applicant data table.'
+                    }
+                    res.send(JSON.stringify(response));
+                } else {
+                    var response = {
+                        status: 300,
+                        message: 'Document not uploaded successfully in applicant data table.'
+                    }
+                    res.send(JSON.stringify(response));
+                }
+            });
+        }
+    });
+});
+app.listen(port);
