f9f474f11c971c6bed5773eafc1348ac7654a0cb
Lakshay Sachdeva <35967999+sachdevlaksh@users.noreply.github.com>
2019-05-06 15:09:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -119,9 +119,9 @@ myApp.controller('myController', ['$scope', 'fileUpload', '$http', '$filter', '$
             $scope.User.user.digitalIdInfo.Age = age;
             var file = $scope.myFile;
             $scope.User.user.digitalIdInfo.documentDetails.docName = file.name;
-            $scope.User.user.GovermentId = $scope.User.digitalIdInfo.GovermentId;
+            $scope.User.user.GovermentId = $scope.User.digitalIdInfo.user.GovermentId;
             $scope.User.user.message = "Record inserted successfully in Cloudant DB.";
-            console.log($scope.User.digitalIdInfo);
+            console.log($scope.User.user.digitalIdInfo);
             var uploadUrl = "/applicantData";
             fileUpload.uploadFileAndFieldsToUrl(file, $scope.User, uploadUrl);
                       
